89647d650c9fac58a89ce6b3c9049794d5c39ccf
Janneison Galindo <janneisongalindo@gmail.com>
2017-12-05 14:29:02 -0500

    cambios heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pwork.wsgi --log-file -
--- pwork/settings.py
+++ pwork/settings.py
@@ -102,14 +102,18 @@ ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = { 'default': dj_database_url.config() }
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
